881d01c011ec6e859a7499629f39a652dee8e09b
unknown <nida.jawre@gmail.com>
2019-08-17 14:38:25 +0530

    initial commit

--- Test.py
+++ Test.py
@@ -1,4 +1,6 @@
 def add1(num):
     return num+1
+def square(num):
+    return num**2
 if __name__ == "__main__":
     print(add1(1))
---
+++ Test2.py
@@ -0,0 +1,4 @@
+def add2(num):
+    return num+2
+if __name__ == "__main__":
+    print(add2(1))
